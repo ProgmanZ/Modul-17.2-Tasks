ce43febffa4c6db71062a972210627a0764ea89f
ProgmanZ <dumphill@gmail.com>
2021-12-07 23:54:10 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - Готово.

--- Task-03.py
+++ Task-03.py
@@ -1,15 +1,25 @@
 # Задача 3. Повышение цен
 
+def print_prise(list_prise):
+    price_string = ''
+    for part in list_prise:
+        price_string += str(part) + ' '
+    return price_string
+
+
 product = []
 for i in range(5):
     product.append(float(input(f'Цена на товар № {i + 1}: ')))
 
-first_year_percent = int(input('Повышение на первый год: '))
+first_year_percent = int(input('\nПовышение на первый год: '))
 second_year_percent = int(input('Повышение на второй год: '))
 
-first_year_prises = [round(first_year_percent, 2) * 0.1 * i for i in product]
-second_year_prises = [round(second_year_percent, 2) * 0.1 * i for i in product]
+first_year_prises = [first_year_percent / 100 * i + i for i in product]
+second_year_prises = [second_year_percent / 100 * i + i for i in product]
 
-sum_prices = [round(first_year_prises[i]+second_year_prises[i], 2) for i in range(5)]
+all_prices = [product, first_year_prises, second_year_prises]
+all_prices = [round(sum(i), 2) for i in all_prices]
 
-print('Сумма цен за каждый год:', sum_prices)
+print_prise(all_prices)
+
+print('\nСумма цен за каждый год: ' + print_prise(all_prices))
