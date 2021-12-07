f5adeb28c97302fa82be3990a6e631cb660eec83
dumphill@gmail.com <Py956jch>
2021-12-07 19:41:25 +0300

    [upd]

--- Task-01.py
+++ Task-01.py
@@ -1,2 +1,10 @@
 # Задача 1. Кубы и квадраты
 
+left = int(input('Левая граница: '))
+right = int(input('Правая граница: '))
+
+qube_list = [i ** 3 for i in range(left, right + 1)]
+quatro_list = [i ** 2 for i in range(left, right + 1)]
+
+print('\nСписок кубов чисел в диапазоне от ', right, 'до', left, ':', qube_list)
+print('Список квадратов чисел в диапазоне от ', right, 'до', left, ':', quatro_list)
